088b3e0a1a42b8ccdc37f4dd464735729b3c49ba
Andrew Mellevo <mellevoleandrew@gmail.com>
2023-10-28 18:49:42 +0800

    Location Buset

--- app/src/main/java/com/example/resqapp/MainActivity.java
+++ app/src/main/java/com/example/resqapp/MainActivity.java
@@ -1,22 +1,44 @@
 package com.example.resqapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresPermission;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String SHARED_PREFS = "sharedPrefs";
 
-    Button locationsharing;
+public class MainActivity extends AppCompatActivity{
+    public static final String SHARED_PREFS = "sharedPrefs";
+    private static final int REQUEST_LOCATION=1;
+
+    Button locationSharing;
+    LocationManager locationManager;
+    TextView currentLoc;
+    String latitude, longitude;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -24,20 +46,93 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
 
-        locationsharing = findViewById(R.id.location_tracking);
+        locationSharing = findViewById(R.id.location_tracking);
+        currentLoc = findViewById(R.id.currentloc);
 
-        locationsharing.setOnClickListener((v) -> {
-            startActivity(new Intent(getApplicationContext(), locationsharing.class));
+        ActivityCompat.requestPermissions(this,new String[]
+                {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        locationSharing.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View v) {
+                locationManager=(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                    OnGps();
+                }
+                else{
+                    getLocation();
+                }
+            }
         });
-
-
-
     }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }else{
+            Location LocationGps= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location LocationNetwork = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Location LocationPassive = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            if(LocationGps != null){
+                double lat=LocationGps.getLatitude();
+                double longi=LocationGps.getLongitude();
+
+                latitude=String.valueOf(lat);
+                longitude=String.valueOf(longi);
+
+                currentLoc.setText("Your Location:"+ "\n"+ "Latitude= " + latitude+ "\n"+ "Longitude= " + longitude);
+            }
+            else  if(LocationNetwork != null){
+                double lat=LocationNetwork.getLatitude();
+                double longi=LocationNetwork.getLongitude();
+
+                latitude=String.valueOf(lat);
+                longitude=String.valueOf(longi);
+
+                currentLoc.setText("Your Location:"+ "\n"+ "Latitude= " + latitude+ "\n"+ "Longitude= " + longitude);
+            }
+            else  if(LocationPassive != null){
+                double lat=LocationPassive.getLatitude();
+                double longi=LocationPassive.getLongitude();
+
+                latitude=String.valueOf(lat);
+                longitude=String.valueOf(longi);
+
+                currentLoc.setText("Your Location:"+ "\n"+ "Latitude= " + latitude+ "\n"+ "Longitude= " + longitude);
+            }
+            else{
+                Toast.makeText(this, "Can't get your location", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void OnGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog=builder.create();
+        alertDialog.show();
+    }
+
     public void logout(View view) {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -47,4 +142,5 @@ public class MainActivity extends AppCompatActivity {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(), Login.class));
     }
+
 }
