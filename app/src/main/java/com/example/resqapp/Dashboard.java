f3d7bcd1ae25bf3dd27fa41a6b9d9c94ca81dc4f
Jennie Precious <preciousclaudia555@gmail.com>
2023-10-30 03:04:26 +0800

    hi

---
+++ app/src/main/java/com/example/resqapp/Dashboard.java
@@ -0,0 +1,44 @@
+package com.example.resqapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+
+public class Dashboard extends AppCompatActivity {
+    TextView email;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_fire_dept);
+         email = findViewById(R.id.email1);
+
+         fAuth = FirebaseAuth.getInstance();
+         fStore = FirebaseFirestore.getInstance();
+
+         userId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                email.setText(documentSnapshot.getString("email"));
+            }
+        });
+
+    }
+
+}
