56cdfd0ece0ea3b8672368a858a47b28366c365c
Jennie Precious <preciousclaudia555@gmail.com>
2023-11-04 16:30:31 +0800

    TAPOS NA OCR!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,10 +44,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
-    implementation("com.squareup.okhttp3:okhttp:3.14.9")
+    implementation ("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.android.gms:play-services-maps:17.0.1")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.karumi:dexter:6.2.1")
-
+    implementation("com.google.firebase:firebase-core:21.1.1")
+    implementation("com.google.firebase:firebase-ml-vision:15.0.0")
     implementation("com.android.volley:volley:1.2.0")
 }
--- app/src/main/java/com/example/resqapp/Ocr.java
+++ app/src/main/java/com/example/resqapp/Ocr.java
@@ -1,17 +1,15 @@
 package com.example.resqapp;
 
-import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,40 +17,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.FormBody;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.util.List;
 
 public class Ocr extends AppCompatActivity {
 
-    private final OkHttpClient okHttpClient = new OkHttpClient();
     private ImageView imageView6;
-    private EditText ocrconverted;
-    private Uri filePath;
-    private final int PICK_IMAGE_REQUEST = 22;
-
+    private TextView ocrconverted;
+    private Button buttonUpload, buttonCapture;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     FirebaseStorage firebaseStorage;
     StorageReference storageReference;
+    private Bitmap imageBitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ocr);
-        Button buttonUpload = findViewById(R.id.upload);
-        Button buttonCapture = findViewById(R.id.capture);
+        buttonUpload = findViewById(R.id.upload);
+        buttonCapture = findViewById(R.id.capture);
         ocrconverted = findViewById(R.id.ocrconverted);
         imageView6 = findViewById(R.id.imageView6);
 
@@ -62,121 +51,79 @@ public class Ocr extends AppCompatActivity {
         buttonUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectImage();
+                dispatchTakePictureIntent();
+                ocrconverted.setText("");
             }
         });
 
         buttonCapture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uploadImage();
+                detectTextFromImage();
             }
         });
 
     }
 
-    private void selectImage(){
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select image from here..."), PICK_IMAGE_REQUEST);
 
 
-    }
 
-    private void uploadImage(){
-        if(filePath != null){
-            ProgressDialog progressDialog = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading...");
-            progressDialog.show();
-
-            StorageReference ref = storageReference.child("images/image");
-
-            ref.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    progressDialog.dismiss();
-                    Toast.makeText(Ocr.this, "Image Uploaded!", Toast.LENGTH_SHORT).show();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    progressDialog.dismiss();
-                    Toast.makeText(Ocr.this, "Failed", Toast.LENGTH_SHORT).show();
-                }
-            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
-
-                    double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
-                    progressDialog.setMessage("Uploaded" + (int) progress + "%");
-                }
-            });
-
-            StorageReference storageReference = FirebaseStorage.getInstance().getReference();
-            StorageReference dateRef = storageReference.child("image/image");
-            dateRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                @Override
-                public void onSuccess(Uri uri) {
-                    Toast.makeText(Ocr.this, uri.toString(), Toast.LENGTH_SHORT).show();
-                    int SDK_INT = Build.VERSION.SDK_INT;
-                    if(SDK_INT >8){
-                        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                        StrictMode.setThreadPolicy(policy);
-                    }
-                    try{
-                        sendPost(uri.toString());
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
-    }
-
-    private void sendPost(String imageUrl) throws Exception{
-        RequestBody formBody = new FormBody.Builder()
-                .add("language", "eng")
-                .add("IsOverlayRequired", "false")
-                .add("url",imageUrl)
-                .add("iscreatesearchablepdf", "false")
-                .add("issearchablepdfhidetextlayer", "false")
-                .build();
-        okhttp3.Request request = new Request.Builder()
-                .url("https://api.ocr.space/parse/image")
-                .addHeader("User-agent", "Okhttp Bot")
-                .addHeader("apikey", "K84173488788957")
-                .post(formBody)
-                .build();
-
-        try(Response response = okHttpClient.newCall(request).execute()){
-            if(!response.isSuccessful()) throw new IOException("Unexpected Code" + response);
-
-            String res= response.body().string();
-            JSONObject obj = new JSONObject(res);
-            JSONArray jsonArray = (JSONArray) obj.get("ParsedResults");
-            for(int i = 0; i<jsonArray.length(); i++){
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                ocrconverted.setText(jsonObject.get("ParsedText").toString());
-            }
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e){
 
         }
+
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
-            filePath = data.getData();
-            try{
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                imageView6.setImageBitmap(bitmap);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+                Bundle extras = data.getExtras();
+                imageBitmap = (Bitmap) extras.get("data");
+                imageView6.setImageBitmap(imageBitmap);
         }
     }
 
+    private void detectTextFromImage() {
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
+
+        firebaseVisionTextDetector.detectInImage(firebaseVisionImage)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        displayTextFromImage(firebaseVisionText);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Ocr.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+    private void displayTextFromImage(FirebaseVisionText firebaseVisionText) {
+        List<FirebaseVisionText.Block> blockList = firebaseVisionText.getBlocks();
+        if(blockList.size()==0){
+            Toast.makeText(this, "No text found in Image", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            for(FirebaseVisionText.Block block : firebaseVisionText.getBlocks()){
+                String text = block.getText();
+                ocrconverted.setText(text);
+            }
+
+        }
+    }
+
+
 }
+
+
+
