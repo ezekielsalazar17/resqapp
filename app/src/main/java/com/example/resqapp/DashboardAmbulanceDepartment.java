9dd4512719f358667ed3c3c56de8c11b0189e3b3
Precious <preciousclaudia555@gmail.com>
2024-01-30 01:48:05 +0800

    dashboard_admins

---
+++ app/src/main/java/com/example/resqapp/DashboardAmbulanceDepartment.java
@@ -0,0 +1,14 @@
+package com.example.resqapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardAmbulanceDepartment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_ambulance_department);
+    }
+}
---
+++ app/src/main/java/com/example/resqapp/DashboardPoliceDepartment.java
@@ -0,0 +1,14 @@
+package com.example.resqapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardPoliceDepartment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboardpolicedepartment);
+    }
+}
