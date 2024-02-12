e788d1c5f5a6803a82715a2f42ba654437b6aa33
Andrew Mellevo <mellevoleandrew@gmail.com>
2024-02-13 00:57:31 +0800

    Paayos Camera

--- app/src/main/java/com/example/resqapp/UserProfile.java
+++ app/src/main/java/com/example/resqapp/UserProfile.java
@@ -2,6 +2,7 @@ package com.example.resqapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,22 +10,29 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class UserProfile extends AppCompatActivity {
 
     Button Logout;
+    ImageView selectimg;
+    FloatingActionButton select;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        setContentView(R.layout.profile);
+        setContentView(R.layout.profile_user);
 
         Logout = findViewById(R.id.logoutuser);
+        selectimg = findViewById(R.id.selectimg);
+        select = findViewById(R.id.selectprofile);
 
         Logout.setOnClickListener(new View.OnClickListener() {
             @Override
